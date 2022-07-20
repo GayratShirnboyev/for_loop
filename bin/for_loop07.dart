247200b113dd87cb9a56e4189fd65ea3e5da71b9
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-20 16:35:54 +0500

    sg7

--- bin/for_loop07.dart
+++ bin/for_loop07.dart
@@ -1,4 +1,3 @@
-int func(List<int> numbers, int k, int n) {
 /*
    Given the list of numbers, return the sum of the even numbers between k and n in the list
 Args:
@@ -8,7 +7,16 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+int func(List<int> numbers, int k, int n) {
+  int f = 0;
+  for (int h = k; h < n; h++) {
+    if (numbers[h] % 2 == 0) {
+      f += numbers[h];
+    }
+  }
+  return f;
 }
 
-void main() {}
+void main() {
+  print(func([1, 3, 5, 6, 7, 8, 8, 5], 1, 6));
+}
