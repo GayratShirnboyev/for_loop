fd575ca38e96f7f7d12bc840dcf153ace7c7453b
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-20 10:47:56 +0500

    dsffi4

--- bin/for_loop04.dart
+++ bin/for_loop04.dart
@@ -1,4 +1,3 @@
-int func(List<int> numbers) {
 /*
    Given the list of numbers, return the sum the even numbers in the list
 Args:
@@ -6,7 +5,33 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+
+int func(List<int> q) {
+  int d = 0;
+  for (int h = 0; h < q.length; h += 1) {
+    if (q[h].isEven) {
+      d = d + q[h];
+    }
+  }
+  return d;
 }
 
-void main() {}
+void main() {
+  print(func([1, 2, 3, 4, 5, 6]));
+}
+  // List<int> lst = [12, 3, 4, 5];
+  // print(lst[1]);
+
+  // if (true) {
+  //   print('printdagi true');
+  // }
+
+  // > < >= <= ==
+  // print('gayrat'.length >= 'sanjarbek'.length);
+
+  // String name = "sanasdfasfjar";
+  // if ('sanjar' == name) {
+  //   print(name);
+  // } else {
+  //   print('sanjar ismi emas');
+  // }
