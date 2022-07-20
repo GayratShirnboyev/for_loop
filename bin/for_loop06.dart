a8d3bf4f2a8414920ad6cf262d261126e8aa7792
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-20 16:24:19 +0500

    ddfg6

--- bin/for_loop06.dart
+++ bin/for_loop06.dart
@@ -1,14 +1,27 @@
-int func(List<int> numbers, int k, int n) {
+// List<int> y = [1, 2, 3, 4, 5, 6, 7];
+// for (int d = 0; d < y.length; d += 1) {
+//   print(y[d]);
+// }
+
 /*
    Given the list of numbers, return the sum of the numbers between k and n in the list
 Args:
   numbers(List): value
-  int: k
+  int: k 
   int: n
-Return: 
+
+Return:  
   Int: answer
 */
-  return 0;
+
+int func(List<int> numbers, int k, int n) {
+  int f = 0;
+  for (int i = k; i < n; i++) {
+    f += numbers[i];
+  }
+  return f;
 }
 
-void main() {}
+void main() {
+  print(func([1, 2, 3, 4, 5, 6, 7], 1, 4));
+}
