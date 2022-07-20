38f77ca8cdcc0517dd5b2f792e6c49d4cc6bc745
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-20 10:49:36 +0500

    dsffi5

--- bin/for_loop05.dart
+++ bin/for_loop05.dart
@@ -1,4 +1,3 @@
-int func(List<int> numbers) {
 /*
    Given the list of numbers, return sum the odd numbers in the list
 Args:
@@ -6,7 +5,28 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+
+int func(List<int> q) {
+  int d = 0;
+  for (int h = 0; h < q.length; h += 1) {
+    if (q[h].isOdd) {
+      d = d + q[h];
+    }
+  }
+  return d;
 }
 
-void main() {}
+void main() {
+  print(func([
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    8,
+    9,
+    9,
+  ]));
+}
